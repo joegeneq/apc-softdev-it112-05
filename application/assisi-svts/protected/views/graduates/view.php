1cb9e48056799691c1d95cd0d48fa3f06dacfc8c
Jeff Almazan <jtalmazan113@gmail.com>
2015-03-20 21:52:00 -0700

    Graduates table CRUD.

---
+++ application/assisi-svts/protected/views/graduates/view.php
@@ -0,0 +1,34 @@
+<?php
+/* @var $this GraduatesController */
+/* @var $model Graduates */
+
+$this->breadcrumbs=array(
+	'Graduates'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array('label'=>'List Graduates', 'url'=>array('index')),
+	array('label'=>'Create Graduates', 'url'=>array('create')),
+	array('label'=>'Update Graduates', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Graduates', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Graduates', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Graduates #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'name',
+		'name_of_school',
+		'year_started_of_scholarship',
+		'year_graduated',
+		'course_title',
+		'honors_recieved',
+		'current_status',
+		'current_employment',
+	),
+)); ?>
