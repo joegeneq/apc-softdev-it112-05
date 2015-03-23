6d1fee53941fa112f1431d28055f13a7cae42908
Rodolfe Espartero <rodolfechristianespartero@gmail.com>
2015-03-24 00:23:33 +0800

    Updated menus for school table.

--- application/assisi-svts/protected/views/school/admin.php
+++ application/assisi-svts/protected/views/school/admin.php
@@ -9,15 +9,11 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'Scholars','url'=>array('profile/admin','type'=>'Student')),
-
+	array('label'=>'Graduates','url'=>array('profile/admin','type'=>'Alumni')),
 	array('label'=>'Schools','url'=>array('school/admin'),'active'=>true),
 	array('label'=>'Grades','url'=>array('grades/admin')),
-	array('label'=>'Sponsors','url'=>array('sponsor/admin')),
 	);
 
-$this->report=array(
-	array('label'=>'Allocations','url'=>array('profile/printIndex')),
-	);
 
 Yii::app()->clientScript->registerScript('search', "
 	$('.search-button').click(function(){
--- application/assisi-svts/protected/views/school/create.php
+++ application/assisi-svts/protected/views/school/create.php
@@ -9,15 +9,11 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'Scholars','url'=>array('profile/admin','type'=>'Student')),
-	array('label'=>'Coordinators','url'=>array('profile/admin','type'=>'Coordinator')),
+	array('label'=>'Graduates','url'=>array('profile/admin','type'=>'Alumni')),
 	array('label'=>'Schools','url'=>array('school/admin'),'active'=>true),
 	array('label'=>'Grades','url'=>array('grades/admin')),
-	array('label'=>'Sponsors','url'=>array('sponsor/admin')),
 	);
 
-$this->report=array(
-	array('label'=>'Allocations','url'=>array('profile/printIndex')),
-	);
 ?>
 
 <h1>Create School</h1>
